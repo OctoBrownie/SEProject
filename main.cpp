2b7c86934b3fe6908cf5182dd9cefe77839aaf73
Crystal <46551461+OctoBrownie@users.noreply.github.com>
2023-11-04 22:04:55 -0700

    Small formatting changes made while looking at the codebase

--- main.cpp
+++ main.cpp
@@ -18,48 +18,47 @@
 
 
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
     QApplication app (argc, argv);
 
-    //The layout of the editor portion of the layout application
+    // The layout of the editor portion of the layout application
     QVBoxLayout* playlistEditor = new QVBoxLayout;
     playlistEditor->setAlignment(Qt::AlignTop);
 
-    //The layout of the MP3 viewr portion of the layout application
+    // The layout of the MP3 viewer portion of the layout application
     QVBoxLayout* MP3Viewer = new QVBoxLayout;
     MP3Viewer->setAlignment(Qt::AlignTop);
 
-    //The layout of the metadata section of the application
+    // The layout of the metadata section of the application
     QHBoxLayout* metadataHolder = new QHBoxLayout;
 
-    //The picture data and the text data in the UI
+    // The picture data and the text data in the UI
     PictureBox* photoData = new PictureBox;
     TextMetadata* textData = new TextMetadata;
 
-    //Where the songs will eventually be listed
+    // Where the songs will eventually be listed
     QScrollArea* songsList = new QScrollArea;
 
 
-    //Buttons that allow playlist editing
+    // Buttons that allow playlist editing
     EditingButtons* playlistEditorButtons = new EditingButtons;
 
-    //Buttons that provide central functionality to the application
+    // Buttons that provide central functionality to the application
     ApplicationButtons* applicationButtons = new ApplicationButtons;
 
-    //Add buttons to viewer
+    // Add buttons to viewer
     MP3Viewer->addLayout(applicationButtons);
 
-    //Add data to the holder component
+    // Add data to the holder component
     metadataHolder->addLayout(photoData);
     metadataHolder->addLayout(textData);
 
-    //Add data to the editor component
+    // Add data to the editor component
     playlistEditor->addLayout(metadataHolder);
     playlistEditor->addLayout(playlistEditorButtons);
     playlistEditor->addWidget(songsList);
 
-    //Make widgets that are designed based on editor and MP3 player
+    // Make widgets that are designed based on editor and MP3 player
     QWidget* sideWidget = new QWidget;
     sideWidget->setLayout(playlistEditor);
 
@@ -68,12 +67,12 @@ int main(int argc, char **argv)
 
 
 
-    //Set the editor pane to the be the playlist editor
+    // Set the editor pane to the be the playlist editor
     QDockWidget* playlistEditPane = new QDockWidget;
     playlistEditPane->setWidget(sideWidget);
     playlistEditPane->setMinimumWidth(500);
 
-    //Compile the interface
+    // Compile the interface
     Interface* mainWindow = new Interface(window, playlistEditPane);
 
     mainWindow->show();
