bbef7a6ea0ac2b44bcb4fc9259be267d2e26303d
Crystal <46551461+OctoBrownie@users.noreply.github.com>
2023-11-21 17:14:33 -0800

    Added some basic fields to the equalizer

--- include/equalizer.h
+++ include/equalizer.h
@@ -1,12 +1,25 @@
 #ifndef EQUALIZER_H
 #define EQUALIZER_H
 
+#include<fftw3.h>
 
 class Equalizer {
 private:
-	float MAX_MULT = 5;
+	float EQUALIZER_MAX_MULT = 5;
+	float lowMult, midMult, highMult;
+
 public:
 	Equalizer();
+	Equalizer(float low, float mid, float high);
+
+	float getLowMult() { return lowMult; }
+	void setLowMult(float f);
+
+	float getMidMult() { return midMult; }
+	void setMidMult(float f);
+
+	float getHighMult() { return highMult; }
+	void setHighMult(float f);
 };
 
 #endif // EQUALIZER_H
--- src/equalizer.cpp
+++ src/equalizer.cpp
@@ -1,13 +1,30 @@
+#include<fftw3.h>
+
 #include "equalizer.h"
 
 Equalizer::Equalizer() {
 	// checking FFTW works
 
-	// fftw_complex *in, *out;
-	// fftw_plan p;
-	// in = (fftw_complex*) fftw_malloc(sizeof(fftw_complex)*10);
-	// out = (fftw_complex*) fftw_malloc(sizeof(fftw_complex)*10);
+	fftw_complex *in, *out;
+	fftw_plan p;
+	in = (fftw_complex*) fftw_malloc(sizeof(fftw_complex)*10);
+	out = (fftw_complex*) fftw_malloc(sizeof(fftw_complex)*10);
 
-	// fftw_free(in);
-	// fftw_free(out);
+	fftw_free(in);
+	fftw_free(out);
+}
+
+void Equalizer::setLowMult(float f) {
+	if (f < 0 || f > EQUALIZER_MAX_MULT) return;
+	lowMult = f;
+}
+
+void Equalizer::setMidMult(float f) {
+	if (f < 0 || f > EQUALIZER_MAX_MULT) return;
+	midMult = f;
+}
+
+void Equalizer::setHighMult(float f) {
+	if (f < 0 || f > EQUALIZER_MAX_MULT) return;
+	highMult = f;
 }
